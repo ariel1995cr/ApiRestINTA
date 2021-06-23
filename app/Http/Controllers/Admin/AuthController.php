b0413d98851ac185d7309b00fdebef60aa5810d0
Sergio ariel triviño <arielrnr1995@gmail.com>
2021-06-23 11:38:35 -0300

    simple paginate user

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -16,7 +16,7 @@ class AuthController extends Controller
         $this->usuario = new User();
     }
     public function index(){
-        return $this->usuario::paginate(10);
+        return $this->usuario::simplePaginate(10);
     }
 
     public function login(Request $request)
